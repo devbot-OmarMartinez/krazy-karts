c2eddf0e2453e62c4405d1759ffcab731ffa9ded
Omar Martinez <fmartinez@devbot.com>
2024-07-10 11:12:40 -0600

    Server functions and cheat protection

--- Source/KrazyKarts/GoKart.h
+++ Source/KrazyKarts/GoKart.h
@@ -53,9 +53,11 @@ private:
 
 	FVector Velocity;
 
-	void MoveForward(float Value);
+	UFUNCTION(Server, Reliable, WithValidation)
+	void Server_MoveForward(float Value);
 
-	void MoveRight(float Value);
+	UFUNCTION(Server, Reliable, WithValidation)
+	void Server_MoveRight(float Value);
 
 	void UpdateLocationFromVelocity(float DeltaTime);
 
